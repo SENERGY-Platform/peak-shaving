53c587b02db19df71941030f1a4d2013a5f2801e
Christoph Schrade <schrade.christoph@gmail.com>
2024-06-18 14:30:00 +0200

    load model only once

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ class Operator(OperatorBase):
 
         self.device_id = None
         self.job_id = None
+        self.model = None
 
         self.historic_data_available = None
         self.training_started = None
@@ -185,7 +186,7 @@ class Operator(OperatorBase):
         if self.historic_data_available and current_timestamp < pd.Timestamp.now() and not self.training_started:
             self.start_training(current_timestamp)
             self.training_started = True
-        if self.job_id and self.is_job_ready():
+        if self.job_id and self.is_job_ready() and self.model:
             min_boundaries, max_boundaries = self.load_model()
             util.logger.debug(f"Min boundaries: {min_boundaries}      Max boundaries: {max_boundaries}")
         new_point = data['Power']
