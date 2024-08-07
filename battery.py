b096126ec133e1ddbdbd000b4f0368b37465a4b0
Christoph Schrade <schrade.christoph@gmail.com>
2024-08-07 11:52:23 +0200

    outsource internal battery decisions to battery device

--- battery.py
+++ battery.py
@@ -1,23 +1,3 @@
 class Battery():
     def __init__(self, capacity):
         self.capacity = capacity
-    
-    def charge(self, power):
-        if power >= self.max_power:
-            power = self.max_power
-        if self.capacity + power >= self.max_capacity:
-            power = self.max_capacity - self.capacity
-            self.capacity = self.max_capacity
-        else:
-            self.capacity += power
-        return power
-    
-    def discharge(self, power):
-        if power >= self.max_power:
-            power = self.max_power
-        if self.capacity - power <= 0:
-            power = self.capacity
-            self.capacity = 0
-        else:
-            self.capacity -= power
-        return power
